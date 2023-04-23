ab2b4ac709d1b775435b2e9af235c1a7de3d9f3c
Pallab Panda <pallab.panda@gmail.com>
2023-04-23 21:57:57 +0530

    operator and expression demo

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,18 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello , Pallab");
+        boolean IsAlien = false;
+        if (!IsAlien) {
+            System.out.println("I am not an Alien");
+            System.out.println("I am scared of Alien");
+        }
+
+        String carModel = "Maruti";
+        boolean isCar = (carModel == "Maruti") ? true : false;
+        if(isCar){
+            System.out.println("This is Maruti");
+        }
+
+
+    }
+}
